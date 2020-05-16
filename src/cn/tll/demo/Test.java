e4e332f91fbd9bbbc30e02893753d949cd55f50e
Tll516330 <2289091393@qq.com>
2020-05-16 10:34:53 +0800

    添加了“随机数遍历循环”

--- src/cn/tll/demo/Test.java
+++ src/cn/tll/demo/Test.java
@@ -1,7 +1,16 @@
 package cn.tll.demo;
 
+import java.util.Random;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("田亮亮最帅");
+        Random random = new Random();
+        //取值1~10之间
+        int num = random.nextInt(10)+1;
+        System.out.println(num);
+        for (int i = 1; i <= num; i++) {
+            System.out.println("田亮亮最帅"+i);
+        }
+
     }
 }
